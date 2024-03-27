d91fcc8bbc82e41d303825710357f6d0604cca9c
basit1215 <haizabdulbasit225@gmail.com>
2024-03-27 07:52:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-// var multiDimensionalArrays = [[], [] ,[]];
-// multiDimensionalArrays[0]=[0, 5, 2, 1];
-// multiDimensionalArrays[1]=[2,1,0,3];
-// multiDimensionalArrays[2]=[2,1,0,4];
+var multiDimensionalArrays = [[], [] ,[]];
+multiDimensionalArrays[0]=[0, 5, 2, 1];
+multiDimensionalArrays[1]=[2,1,0,3];
+multiDimensionalArrays[2]=[2,1,0,4];
 
-// document.write(multiDimensionalArrays[0].join(" ")+ "<br>" );
-// document.write(multiDimensionalArrays[1].join(" ")+ "<br>" );
-// document.write(multiDimensionalArrays[2].join(" ")+ "<br>" + "<br>");
+document.write(multiDimensionalArrays[0].join(" ")+ "<br>" );
+document.write(multiDimensionalArrays[1].join(" ")+ "<br>" );
+document.write(multiDimensionalArrays[2].join(" ")+ "<br>" + "<br>");
 
-// for (i=1; i<=10; i++){
-//     document.write(i+ "<br>"+ "<br>");
-// }
+for (i=1; i<=10; i++){
+    document.write(i+ "<br>"+ "<br>");
+}
 
-// var reqNumberOfTable= +prompt("Enter A Number For Multiplication");
-// var lengthOfTable= +prompt("Enter A Length Of Table");
-// document.write("<b>Multiplication Table of " + reqNumberOfTable + "</b>" + "<br>");
-// document.write("<b>Length " +lengthOfTable + "</b>"+ "<br>"+ "<br>");
-// // let flag = false;
-// if (isNaN(reqNumberOfTable) || 
-// isNaN(lengthOfTable) ){
-//       alert("You Entered an Invalid Number ");
-//     }
-// else{
-//     for(var i=1; i<=lengthOfTable; i++){
-//     document.write(reqNumberOfTable+ " x " + i + " = " + reqNumberOfTable * i + "<br>"  );
-// }
-// }
+var reqNumberOfTable= +prompt("Enter A Number For Multiplication");
+var lengthOfTable= +prompt("Enter A Length Of Table");
+document.write("<b>Multiplication Table of " + reqNumberOfTable + "</b>" + "<br>");
+document.write("<b>Length " +lengthOfTable + "</b>"+ "<br>"+ "<br>");
+// let flag = false;
+if (isNaN(reqNumberOfTable) || 
+isNaN(lengthOfTable) ){
+      alert("You Entered an Invalid Number ");
+    }
+else{
+    for(var i=1; i<=lengthOfTable; i++){
+    document.write(reqNumberOfTable+ " x " + i + " = " + reqNumberOfTable * i + "<br>"  );
+}
+}
 
-// var fruits= ["Apple", "Banana", "Mango", "Orange", "Strawberry"];
-// for(i=0; i<fruits.length; i++){
-//     document.write(fruits[i]+ "<br>");
-// }
-// for(i=0; i<fruits.length; i++){
-//     document.write("Element at index " + i + " is " + fruits[i]+ "<br>"+ "<br>");
-// }
+var fruits= ["Apple", "Banana", "Mango", "Orange", "Strawberry"];
+for(i=0; i<fruits.length; i++){
+    document.write(fruits[i]+ "<br>");
+}
+for(i=0; i<fruits.length; i++){
+    document.write("Element at index " + i + " is " + fruits[i]+ "<br>"+ "<br>");
+}
 
 document.write("Counting: "+ "<br>" );
 for(var i=1; i<=15; i++){
@@ -75,8 +75,6 @@ for(i=0; i<bakery.length; i++)
         alert("We are Sorry... " + userInput + " is not available in our Bakery")
     }
  
-
-
 var largestNumber=[24,53, 78, 91, 12];
 document.write("Array Items: " + largestNumber+ "<br>");
 document.write( "The Largest Number is: " + Math.max(...largestNumber)+ "<br>"+ "<br>");
